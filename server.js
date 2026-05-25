449dfef62c09016c6eb354cf45247e6cbb670d9a
Vishal Shah <34372081+Vishal-B-Shah@users.noreply.github.com>
2026-05-25 22:28:48 +0530

    Fix port for cloud deployment

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors    = require('cors');
 const path    = require('path');
 
 const app  = express();
-const PORT = 5000;
+//const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // ── Open / create database ────────────────────────────────────────────────────
 const db = new sqlite3.Database(path.join(__dirname, 'mangoes.db'), (err) => {
